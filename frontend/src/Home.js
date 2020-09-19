c51dc2ff26d63568125111090f42c429d4fbfee6
vmaguiar <vmaguiar@hotmail.com>
2020-09-19 09:11:07 -0300

    update frontend

---
+++ frontend/src/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import Product from "./Product";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="amazon prime banner"
+        />
+
+        <div className="home_row">
+          <Product
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="12345678"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+
+        <div className="home_row">
+          <Product
+            id="42589746"
+            title="Samsung Watch LC42342CSD Curved LED"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="78546982"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="11453214"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
+
+        <div className="home_row">
+          <Product
+            id="87456123"
+            title="Samsung LC202020 49' Curved LED Gaming Monitor"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./Product.css";
+
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product_info">
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+
+      <img className="product_img" src={image} alt="product" />
+
+      <button className="product_button">Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
